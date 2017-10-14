e1c089e2c3fdf50a1d8c7b1d49c27e5dbb0b791d
GrajewskiPawel <pawel@pawelgrajewski.com.pl>
2017-10-15 00:15:14 +0200

    cleaning script code

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ window.onload = function(){
 		var shop = document.getElementById("shopWraper");
 		var chosen = '';
 		var xPrice = document.getElementById("xPrice"); 
+		var shopFunction = function(){
 		shop.addEventListener("DOMNodeInserted", function (event) {
 			var suma = [];
+			//get prive values of all add element
   			var para = document.getElementsByClassName("price");		
 			for(i = 0; i< para.length; i++){
-				
 				var value  = para[i].innerHTML;
 				suma.push(Number(value));
-
 				var sum = 0;
-				suma.forEach(
-					function addNumber(value) { 
-						sum += value; 
-					}
-				);
+				//math sum for price values
+				suma.forEach(function addNumber(value) { sum += value; });
+				//add sum to sum elemement
 				xPrice.innerHTML = sum;	
 			}			
 		}, false);//on DOMNodeInserted
-		
-		for(var i = 0; i < buttonSent.length; i++){
+		}//and of shop function
+		shopFunction();//function when elemement is add to shop div
+		var addProduct = function(){
+			for(var i = 0; i < buttonSent.length; i++){
 			buttonSent[i].onclick = function(){
 				//seting values to sent
 				var price = "price";
@@ -30,9 +30,11 @@ window.onload = function(){
 				var howMuch = this.name + howMuch;
 				var valueProduct = document.getElementById(price).value;
 				var howMuchProduct = document.getElementById(howMuch).value;
+				//protect for set 0 values
 				if(howMuchProduct < 1){
 					return;
 				}
+				//math on product element
 				var suma = valueProduct * howMuchProduct;
 				//creating delete button
 				var buttonDelete = document.createElement("button");
@@ -40,7 +42,7 @@ window.onload = function(){
 				buttonDelete.classList.add(this.name);
 				buttonDelete.appendChild(buttonDeleteNode);
 				buttonDelete.setAttribute("id",this.name);
-				//creating component to set
+				//creating component to sent to shop wraper
 				var para = document.createElement("p");
 				para.setAttribute("id",this.name);
 				para.setAttribute("data-price",suma);
@@ -49,29 +51,31 @@ window.onload = function(){
 				para.innerHTML = "Price:" + valueProduct + "Quantity:" + howMuchProduct + "Suma" + "<span class='price'>" + suma + "</span>";
 				para.appendChild(buttonDelete);
 				shop.appendChild(para);
+				//event when delete button is clicked
 				buttonDelete.onclick = function(){	
-					var x = this.parentElement;					
+					var x = this.parentElement;	
+					//delete buttn with parent
 					x.remove();
+					//clear sum array
 					var suma = [];
-  			var para = document.getElementsByClassName("price");	
+  					var para = document.getElementsByClassName("price");	
 					if(para.length == 0){
 					xPrice.innerHTML = 0;
 				}
-			for(i = 0; i< para.length; i++){
-				
-				var value  = para[i].innerHTML;
-				suma.push(Number(value));
-
-				var sum = 0;
-				suma.forEach(
-					function addNumber(value) { 
-						sum += value; 
-					}
-				);
-				xPrice.innerHTML = sum;	
-			}	
-				}
-			}
+						for(i = 0; i< para.length; i++){
+						//add price values when one of element is delete
+						var value  = para[i].innerHTML;
+						suma.push(Number(value));
+						var sum = 0;
+						suma.forEach(function addNumber(value) { sum += value; });
+						//add new values to sum element
+						xPrice.innerHTML = sum;	
+					}	
+				};
+			};
 		}
+		}
+		addProduct();//function which is activate when click on product button
 		
-	}
+		
+	};
